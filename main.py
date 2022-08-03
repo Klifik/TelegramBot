c6940262cb5b01cabed3851ffa70a2427c9340e4
Klifik <109215319+Klifik@users.noreply.github.com>
2022-08-03 21:03:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ token = '5581283557:AAGJibdTtVaYsF2DuXxvuYnWPNhfFGqlXpg'
 bot = telebot.TeleBot(token)
 
 #Отрисовка
-def object(URL):
-    markup = types.InlineKeyboardMarkup()
-    buy = types.InlineKeyboardButton('Купить', callback_data='buy')
-    back2 = types.InlineKeyboardButton('Назад', callback_data='back2')
-    url = types.InlineKeyboardButton('Подробная информация', url=URL)
-    markup.add(buy, back2, url)
-    return markup
+
 def gen_markup():
     markup = types.InlineKeyboardMarkup(row_width=2)
     item1 = types.InlineKeyboardButton("Плоскорезы", callback_data="Плоскорезы")
@@ -31,6 +25,38 @@ def message_handler(message):
     bot.send_photo(message.chat.id, URL1, reply_markup=gen_markup())
     global idmessage
     idmessage = message.message_id
+
+#Садовые товары
+def sadmarkup1():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    sp = ['Душ "Степ"', 'Садовый бордюр 100мм', 'Дорожка садовая', 'Наколенники', 'Кустодержатель',
+          'Фруктосборник', 'Пугало-Антикрот', 'Сушилка', 'Стул садовый', 'Скворечник', ]
+    buttons = []
+    c = 0
+    next = types.InlineKeyboardButton('Следущяя страница', callback_data='NextSec')
+    gv = types.InlineKeyboardButton('Главное меню', callback_data= 'gv')
+    for i in range(10):
+        item = types.InlineKeyboardButton(sp[i], callback_data=sp[i])
+        markup.add(item)
+    markup.add(gv, next)
+    return markup
+def sadmarkup2():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    sp = ['Набор для комнатный растений', 'Комби',
+          'Туннельный парник (Парник укрытие)']
+    buttons = []
+    c = 0
+    next = types.InlineKeyboardButton('Предыдущая страница', callback_data='BackSec')
+    gv = types.InlineKeyboardButton('Главное меню', callback_data= 'gv')
+    for i in range(3):
+        item = types.InlineKeyboardButton(sp[i], callback_data=sp[i])
+        markup.add(item)
+    markup.add(gv, next)
+    return markup
+
+
+
+#Плоскорезы
 def plockmarkup():
     markup = types.InlineKeyboardMarkup(row_width=2)
     sp = ['Фокина большой', 'Фокина малый', 'Аист', 'Стриж большой', 'Стриж средний', 'Стриж малый',
@@ -58,15 +84,18 @@ def plockmarkup2():
         markup.add(item)
     markup.add(gv, next)
     return markup
+def object(URL):
+    markup = types.InlineKeyboardMarkup()
+    buy = types.InlineKeyboardButton('Купить', callback_data='buy')
+    back2 = types.InlineKeyboardButton('Назад', callback_data='back2')
+    url = types.InlineKeyboardButton('Подробная информация', url=URL)
+    markup.add(buy, back2, url)
+    return markup
 
 #Обработка
 @bot.callback_query_handler(func=lambda call: True)
 def reload(call):
-    sp = ['Фокина большой', 'Фокина малый', 'Аист', 'Стриж большой', 'Стриж средний', 'Стриж малый',
-          'Стриж укороченный',
-          'Корнеудалитель', 'Краб большой', 'Краб малый']
-    sp2 = ['Дракон', 'Лепесток', 'Трансформер БВГК', 'Почвообрабатывающий агрегат Чибис',
-           'Гидра', 'Тяпка (мотыжка)', 'Плоскорез Z3', 'Плоскорез Z4', 'Аэратор', 'Семейный']
+    from config import sp1, sp2, sp3, sp4
     if call.data == 'Плоскорезы':
         plock(call.message)
     if call.data == 'Next':
@@ -76,7 +105,7 @@ def reload(call):
     if call.data == 'gv':
         gv(call.message)
     for i in range(10):
-        if call.data == sp[i]:
+        if call.data == sp1[i]:
             iter = i
             obj1(call.message, iter)
     for i in range(10):
@@ -87,6 +116,64 @@ def reload(call):
         back2(call.message)
     if call.data == 'Контакты':
         contakts(call.message)
+
+
+
+
+    if call.data == 'Садовые товары':
+        sad(call.message)
+    if call.data == 'NextSec':
+        NextSec(call.message)
+    if call.data == 'BackSec':
+        BackSec(call.message)
+    for i in range(10):
+        if call.data == sp3[i]:
+            iter = i
+            obj3(call.message, iter)
+    for i in range(3):
+        if call.data == sp4[i]:
+            iter = i
+            obj4(call.message, iter)
+
+def contakts(message):
+    bot.send_message(message.chat.id, 'Наш адрес: 601351, Владимирская обл.,г.Судогда,ул. Гагарина, 5.\n'
+                                      'Наши телефоны: 8(49235)2-12-16; 2-25-12;\n'
+                                      'E-mail: srtp@bk.ru; s-ploskorez@gmail.com')
+def gv(message):
+    from config import URL1
+    bot.send_message(message.chat.id,
+                     f' Добро пожаловать в магазин садовых товаров Судогодоское!')
+    bot.send_photo(message.chat.id, URL1, reply_markup=gen_markup())
+    bot.delete_message(message.chat.id, message_id=message.message_id)
+
+
+#Садовые товары
+def sad(message):
+    from config import URL2
+    bot.send_photo(message.chat.id, URL2, reply_markup=sadmarkup1())
+    bot.delete_message(message.chat.id, message_id=message.message_id)
+    bot.delete_message(message.chat.id, message_id=message.message_id-1)
+def NextSec(message):
+    from config import URL2
+    bot.send_photo(message.chat.id, URL2, reply_markup=sadmarkup2())
+    bot.delete_message(message.chat.id, message_id=message.message_id)
+def BackSec(message):
+    from config import URL2
+    bot.send_photo(message.chat.id, URL2, reply_markup=sadmarkup1())
+    bot.delete_message(message.chat.id, message_id=message.message_id)
+def obj3(message, i):
+    from config import ALLTEXT3, ALLURLINF3, ALLURLPIC3
+    bot.send_photo(message.chat.id, ALLURLPIC3[i], caption=ALLTEXT3[i], reply_markup=object(ALLURLINF3[i]))
+    bot.delete_message(message.chat.id, message_id=message.message_id)
+def back3(message):
+    from config import URL2
+    bot.send_photo(message.chat.id, URL2, reply_markup=sadmarkup1())
+    bot.delete_message(message.chat.id, message_id=message.message_id)
+def obj4(message, i):
+    from config import ALLTEXT4, ALLURLINF4, ALLURLPIC4
+    bot.send_photo(message.chat.id, ALLURLPIC4[i], caption=ALLTEXT4[i], reply_markup=object(ALLURLINF4[i]))
+    bot.delete_message(message.chat.id, message_id=message.message_id)
+#Плоскорезы
 def plock(message):
     from config import URL2
     bot.send_photo(message.chat.id, URL2, reply_markup=plockmarkup())
@@ -100,12 +187,6 @@ def back(message):
     from config import URL2
     bot.send_photo(message.chat.id, URL2, reply_markup=plockmarkup())
     bot.delete_message(message.chat.id, message_id=message.message_id)
-def gv(message):
-    from config import URL1
-    bot.send_message(message.chat.id,
-                     f' Добро пожаловать в магазин садовых товаров Судогодоское!')
-    bot.send_photo(message.chat.id, URL1, reply_markup=gen_markup())
-    bot.delete_message(message.chat.id, message_id=message.message_id)
 def obj1(message, i):
     from config import ALLTEXT1, ALLURLINF1, ALLURLPIC1
     bot.send_photo(message.chat.id, ALLURLPIC1[i], caption=ALLTEXT1[i], reply_markup=object(ALLURLINF1[i]))
@@ -118,10 +199,7 @@ def back2(message):
     from config import URL2
     bot.send_photo(message.chat.id, URL2, reply_markup=plockmarkup())
     bot.delete_message(message.chat.id, message_id=message.message_id)
-def contakts(message):
-    bot.send_message(message.chat.id, 'Наш адрес: 601351, Владимирская обл.,г.Судогда,ул. Гагарина, 5.\n'
-                                      'Наши телефоны: 8(49235)2-12-16; 2-25-12;\n'
-                                      'E-mail: srtp@bk.ru; s-ploskorez@gmail.com')
+
 
 
 
